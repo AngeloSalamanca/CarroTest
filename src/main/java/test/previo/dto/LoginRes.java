f45b5f7f0fcb36f5bbfa21d9c42b2a026d606a8c
Angelo Salamanca <angelosteevensm@ufps.edu.co>
2023-12-09 16:45:08 -0500

    Api completada

---
+++ src/main/java/test/previo/dto/LoginRes.java
@@ -0,0 +1,14 @@
+package test.previo.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRes {
+    private String email;
+    private String token;
+
+    public LoginRes(String email, String token) {
+        this.email = email;
+        this.token = token;
+    }
+}
